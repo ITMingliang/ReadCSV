37db0638dcc0d49485eb42f02973065644786fc4
mingliang.wang <mingliang_xyz@outlook.com>
2022-02-22 15:12:26 +0800

    init add.

---
+++ ReadCSV/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ReadCSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboxFiles = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bt_Scan = new System.Windows.Forms.Button();
+            this.bt_Open = new System.Windows.Forms.Button();
+            this.bt_Save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboxFiles
+            // 
+            this.comboxFiles.FormattingEnabled = true;
+            this.comboxFiles.Location = new System.Drawing.Point(148, 52);
+            this.comboxFiles.Name = "comboxFiles";
+            this.comboxFiles.Size = new System.Drawing.Size(346, 20);
+            this.comboxFiles.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(75, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CSV文件：";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(78, 158);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(503, 192);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // bt_Scan
+            // 
+            this.bt_Scan.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.bt_Scan.Location = new System.Drawing.Point(175, 114);
+            this.bt_Scan.Name = "bt_Scan";
+            this.bt_Scan.Size = new System.Drawing.Size(75, 23);
+            this.bt_Scan.TabIndex = 3;
+            this.bt_Scan.Text = "扫描数据";
+            this.bt_Scan.UseVisualStyleBackColor = true;
+            this.bt_Scan.Click += new System.EventHandler(this.bt_Scan_Click);
+            // 
+            // bt_Open
+            // 
+            this.bt_Open.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.bt_Open.Location = new System.Drawing.Point(355, 114);
+            this.bt_Open.Name = "bt_Open";
+            this.bt_Open.Size = new System.Drawing.Size(75, 23);
+            this.bt_Open.TabIndex = 4;
+            this.bt_Open.Text = "打开数据";
+            this.bt_Open.UseVisualStyleBackColor = true;
+            this.bt_Open.Click += new System.EventHandler(this.bt_Open_Click);
+            // 
+            // bt_Save
+            // 
+            this.bt_Save.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.bt_Save.Location = new System.Drawing.Point(175, 394);
+            this.bt_Save.Name = "bt_Save";
+            this.bt_Save.Size = new System.Drawing.Size(75, 23);
+            this.bt_Save.TabIndex = 5;
+            this.bt_Save.Text = "保存数据";
+            this.bt_Save.UseVisualStyleBackColor = true;
+            this.bt_Save.Click += new System.EventHandler(this.bt_Save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bt_Save);
+            this.Controls.Add(this.bt_Open);
+            this.Controls.Add(this.bt_Scan);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboxFiles);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboxFiles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button bt_Scan;
+        private System.Windows.Forms.Button bt_Open;
+        private System.Windows.Forms.Button bt_Save;
+    }
+}
+
---
+++ ReadCSV/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadCSV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //扫描路径
+        DirectoryInfo TheFolder = new DirectoryInfo($"{System.Environment.CurrentDirectory}" + "\\实时数据");
+        string filePath = $"{System.Environment.CurrentDirectory}" + "\\实时数据\\";
+        private void bt_Scan_Click(object sender, EventArgs e)
+        {
+            comboxFiles.Items.Clear();
+            comboxFiles.SelectedIndex = comboxFiles.Items.Count > 0 ? 0 : -1;
+
+            //遍历目录下的文件名
+            foreach (FileInfo NextFile in TheFolder.GetFiles())
+            {
+                this.comboxFiles.Items.Add(NextFile);
+            }
+
+            //选择默认
+            comboxFiles.SelectedIndex = comboxFiles.Items.Count > 0 ? 0 : -1;
+        }
+
+        private void bt_Open_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            var file = File.Open(filePath + comboxFiles.Text, FileMode.Open);
+            List<string> list = new List<string>();
+
+            using (var stream = new StreamReader(file))
+            {
+                while (!stream.EndOfStream)
+                {
+                    list.Add(stream.ReadLine());
+                }
+            }
+
+            var array = new string[list.Count, 3];
+            var line = 0;
+            list.ForEach(item => {
+                var row1 = 0;
+                item.Split(',').ToList().ForEach(p =>
+                {
+                    array.SetValue(p, line, row1);
+                    row1++;
+                });
+                line++;
+            });
+            file.Close();
+
+            //去除标题行，从1行开始
+            for (int i = 1; i < list.Count; i++)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+
+                int index = dataGridView1.Rows.Add(row);
+                dataGridView1.Rows[index].Cells[0].Value = array[i , 0];
+                dataGridView1.Rows[index].Cells[1].Value = array[i , 1];
+                dataGridView1.Rows[index].Cells[2].Value = array[i , 2];
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboxFiles.SelectedIndex = comboxFiles.Items.Count > 0 ? 0 : -1;
+            foreach (FileInfo NextFile in TheFolder.GetFiles())
+            {
+                this.comboxFiles.Items.Add(NextFile);
+            }
+            comboxFiles.SelectedIndex = comboxFiles.Items.Count > 0 ? 0 : -1;
+
+            //添加列
+            DataGridViewTextBoxColumn col0 = new DataGridViewTextBoxColumn();
+            DataGridViewTextBoxColumn col1 = new DataGridViewTextBoxColumn();
+            DataGridViewTextBoxColumn col2 = new DataGridViewTextBoxColumn();
+            col0.HeaderText = "序号";
+            col1.HeaderText = "姓名";
+            col2.HeaderText = "年龄";
+            dataGridView1.Columns.Add(col0);
+            dataGridView1.Columns.Add(col1);
+            dataGridView1.Columns.Add(col2);
+
+            DataGridViewCheckBoxCell cellText = new DataGridViewCheckBoxCell();
+            DataGridViewImageCell cellImage = new DataGridViewImageCell();
+
+        }
+
+        private void bt_Save_Click(object sender, EventArgs e)
+        {
+            bool f = dataGridViewToCSV(dataGridView1);
+        }
+
+  
+        public static bool dataGridViewToCSV(DataGridView dataGridView)
+        {
+            if (dataGridView.Rows.Count == 0)
+            {
+                MessageBox.Show("没有数据可导出!", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return false;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "CSV files (*.csv)|*.csv";
+            saveFileDialog.FilterIndex = 0;
+            saveFileDialog.RestoreDirectory = true;
+            saveFileDialog.AddExtension = true;//设置自动添加文件拓展名
+            saveFileDialog.CreatePrompt = true;//创建提示
+            saveFileDialog.FileName = null;//默认文件名
+            saveFileDialog.Title = "保存";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Stream stream = saveFileDialog.OpenFile();
+                StreamWriter sw = new StreamWriter(stream, System.Text.Encoding.GetEncoding(-0));
+                string strLine = "";
+                try
+                {
+                    //表头
+                    for (int i = 0; i < dataGridView.ColumnCount; i++)
+                    {
+                        if (i > 0)
+                            strLine += ",";
+                        strLine += dataGridView.Columns[i].HeaderText;
+                    }
+                    strLine.Remove(strLine.Length - 1);
+                    sw.WriteLine(strLine);
+                    strLine = "";
+
+                    //表的内容
+                    for (int j = 0; j < dataGridView.Rows.Count; j++)
+                    {
+                        strLine = "";
+                        int colCount = dataGridView.Columns.Count;
+                        for (int k = 0; k < colCount; k++)
+                        {
+                            //以逗号分隔
+                            if (k > 0 && k < colCount)
+                                strLine += ",";
+
+                            if (dataGridView.Rows[j].Cells[k].Value == null)
+                                strLine += "";
+                            else
+                            {
+                                string cell = dataGridView.Rows[j].Cells[k].Value.ToString().Trim();
+                                //防止里面含有特殊符号
+                                cell = cell.Replace("\"", "\"\"");
+                                //cell = "\"" + cell + "\""; //每个元素值用引号包括
+                                strLine += cell;
+                            }
+                        }
+                        sw.WriteLine(strLine);
+                    }
+
+                    sw.Close();
+                    stream.Close();
+                    MessageBox.Show("数据被导出到：" + saveFileDialog.FileName.ToString(), "导出完毕", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "导出错误", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
